fc48e1b5c321ba1a57453f0cb54dffbbfea82ed8
waihlyanaung <hlyan8834@gmail.com>
2023-11-16 08:23:05 +0630

    v2

--- src/routes/View.jsx
+++ src/routes/View.jsx
@@ -1,21 +1,30 @@
-// View.jsx
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
+import React, { useEffect } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
+
 import Home from '../pages/Home';
 import Hello from '../pages/Hello';
 
 const View = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const getTitleForRoute = () => {
+      switch (location.pathname) {
+        case '/':
+          return 'mmmm';
+        case '/hello':
+          return 'bbbb';
+        default:
+          return 'Default Title';
+      }
+    };
+
+    const title = getTitleForRoute();
+    document.title = title;
+  }, [location.pathname]);
+
   return (
     <>
-      <Helmet>
-        <html lang="en" />
-        <meta charset="UTF-8" />
-        <link rel="icon" type="image/svg+xml" href="/vite.svg" />
-        <meta name="viewport" content="hello" />
-        <title>Vite + React</title>
-      </Helmet>
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/hello" element={<Hello />} />
