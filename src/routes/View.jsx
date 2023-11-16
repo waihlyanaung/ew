570943e0778844b48c617722ed67964f163d6a6f
waihlyanaung <hlyan8834@gmail.com>
2023-11-16 08:35:59 +0630

    v2

--- src/routes/View.jsx
+++ src/routes/View.jsx
@@ -1,35 +1,38 @@
-import React, { useEffect } from 'react';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { HelmetProvider, Helmet } from 'react-helmet-async';
 
 import Home from '../pages/Home';
 import Hello from '../pages/Hello';
 
 const View = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    const getTitleForRoute = () => {
-      switch (location.pathname) {
-        case '/':
-          return 'mmmm';
-        case '/hello':
-          return 'bbbb';
-        default:
-          return 'Default Title';
-      }
-    };
-
-    const title = getTitleForRoute();
-    document.title = title;
-  }, [location.pathname]);
-
   return (
-    <>
+    <HelmetProvider>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/hello" element={<Hello />} />
+        <Route
+          path="/"
+          element={
+            <React.Fragment>
+              <Helmet>
+                <title>mmmm</title>
+              </Helmet>
+              <Home />
+            </React.Fragment>
+          }
+        />
+        <Route
+          path="/hello"
+          element={
+            <React.Fragment>
+              <Helmet>
+                <title>bbbb</title>
+              </Helmet>
+              <Hello />
+            </React.Fragment>
+          }
+        />
       </Routes>
-    </>
+    </HelmetProvider>
   );
 };
 
